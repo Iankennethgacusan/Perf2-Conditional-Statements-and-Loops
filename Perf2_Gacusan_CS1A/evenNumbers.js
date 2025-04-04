9b3bbe61df40a5f7c7a28591aeb043050df75583
Iankennethgacusan <ganadoian31@gmail.com>
2025-04-04 23:55:58 +0800

    Create evenNumbers.js

---
+++ Perf2_Gacusan_CS1A/evenNumbers.js
@@ -0,0 +1,9 @@
+console.log("Numbers from 1 to 10:");
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log("\nEven numbers from 1 to 10:");
+for (let i = 2; i <= 10; i += 2) {
+  console.log(i);
+}
